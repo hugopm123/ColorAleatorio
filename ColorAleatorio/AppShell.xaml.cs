38af1e519b2f79868385683d7b52e1d36f625b2f
Hugopm123 <hugorafa@live.com>
2024-12-03 14:46:24 -0600

    Agregar archivos de proyecto.

---
+++ ColorAleatorio/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ColorAleatorio
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ColorAleatorio/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿namespace ColorAleatorio
+{
+    public partial class MainPage : ContentPage
+    {
+        private Random random = new Random();
+        private string currentColorHex = "#FFFFFF";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            myAds.AdsId = "ca-app-pub-7110513556928885/7130570156";
+        }
+
+        private void OnNewColorClicked(object sender, EventArgs e)
+        {
+            Color randomColor = GenerateRandomColor();
+            ApplyRandomColor(randomColor);
+        }
+
+        private Color GenerateRandomColor()
+        {
+            return Color.FromRgb(
+                (byte)random.Next(256),
+                (byte)random.Next(256),
+                (byte)random.Next(256)
+            );
+        }
+
+        private void ApplyRandomColor(Color color)
+        {
+            mainGrid.BackgroundColor = color;
+            currentColorHex = ColorToHex(color);
+            colorLabel.Text = $"Color: {currentColorHex}";
+        }
+
+        private string ColorToHex(Color color)
+        {
+            return $"#{(int)(color.Red * 255):X2}{(int)(color.Green * 255):X2}{(int)(color.Blue * 255):X2}";
+        }
+
+        private async void OnCopyColorClicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(currentColorHex);
+
+        }
+    }
+}
